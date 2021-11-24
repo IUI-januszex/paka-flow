4fb19a676076d545d2c8cb606a226e48654129c2
dawid <dawid_szymkiewicz@hotmail.com>
2021-11-24 19:10:11 +0100

    add workdays service

--- src/main/java/pl/com/januszex/paka/flow/base/DateTimeServiceAdapter.java
+++ src/main/java/pl/com/januszex/paka/flow/base/DateTimeServiceAdapter.java
@@ -3,6 +3,8 @@ package pl.com.januszex.paka.flow.base;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -18,5 +20,24 @@ class DateTimeServiceAdapter implements DateTimeServicePort {
     @Override
     public boolean isToday(LocalDateTime localDateTime) {
         return fmt.format(localDateTime).equals(fmt.format(getNow()));
+    }
+
+    @Override
+    public LocalDate addWorkdays(LocalDate date, int workdays) {
+        if (workdays < 1) {
+            return date;
+        }
+
+        LocalDate result = date;
+        int addedDays = 0;
+        while (addedDays < workdays) {
+            result = result.plusDays(1);
+            if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                    result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                ++addedDays;
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/pl/com/januszex/paka/flow/base/DateTimeServicePort.java
+++ src/main/java/pl/com/januszex/paka/flow/base/DateTimeServicePort.java
@@ -1,9 +1,12 @@
 package pl.com.januszex.paka.flow.base;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface DateTimeServicePort {
     LocalDateTime getNow();
 
     boolean isToday(LocalDateTime localDateTime);
+
+    LocalDate addWorkdays(LocalDate date, int workdays);
 }
